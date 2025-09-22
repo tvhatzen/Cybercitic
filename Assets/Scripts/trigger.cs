c393c534ba735b7fe6fd3026fa30eb93acc2788d
JuliaCarew <crewjulia3@gmail.com>
2025-09-22 09:43:32 -0300

    test, got trigger script set up

---
+++ Assets/Scripts/trigger.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class trigger : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision player)
+    {
+        //if (player.gameObject.tag == “Player”){
+
+        //}
+    }
+}
